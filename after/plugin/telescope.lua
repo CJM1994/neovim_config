1ebf1be2b7bb5dfe532389ac87893f15a1ac7c2e
connormullin <connorjohnmullin@gmail.com>
2023-08-31 00:15:53 -0600

    add git commands for telescopte to hotkeys

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,17 @@
 -- Telescope Remaps
 local builtin = require('telescope.builtin')
 vim.g.mapleader = ' '
+
+-- Fuzzy Search
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+-- Git Commands
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_status, {})
+
+-- Grep
 vim.keymap.set('n', '<leader>gr', function()
   builtin.grep_string({ search = vim.fn.input('Grep > ') })
 end)
--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,105 +1,105 @@
 -- Package Manager Setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.2',
-		dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' }
-	},
-	{
-		'rose-pine/neovim', name = 'rose-pine'
-	},
-	{
-		'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'
-	},
-	{
-		'mbbill/undotree'
-	},
-	{
-		'tpope/vim-fugitive'
-	},
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		dependencies = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' }, -- Required
-			{ 'williamboman/mason.nvim' }, -- Optional
-			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' }, -- Required
-			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
-			{ 'L3MON4D3/LuaSnip' }, -- Required
-		}
-	}
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' }
+    },
+    {
+        'rose-pine/neovim', name = 'rose-pine'
+    },
+    {
+        'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'
+    },
+    {
+        'mbbill/undotree'
+    },
+    {
+        'tpope/vim-fugitive'
+    },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },    -- Required
+            { 'williamboman/mason.nvim' },  -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'L3MON4D3/LuaSnip' },         -- Required
+        }
+    },
 })
 
 -- Load Color Scheme
 require('rose-pine').setup({
-	--- @usage 'auto'|'main'|'moon'|'dawn'
-	variant = 'auto',
-	--- @usage 'main'|'moon'|'dawn'
-	dark_variant = 'main',
-	bold_vert_split = false,
-	dim_nc_background = false,
-	disable_background = false,
-	disable_float_background = false,
-	disable_italics = false,
+    --- @usage 'auto'|'main'|'moon'|'dawn'
+    variant = 'auto',
+    --- @usage 'main'|'moon'|'dawn'
+    dark_variant = 'main',
+    bold_vert_split = false,
+    dim_nc_background = false,
+    disable_background = false,
+    disable_float_background = false,
+    disable_italics = false,
 
-	--- @usage string hex value or named color from rosepinetheme.com/palette
-	groups = {
-		background = 'base',
-		background_nc = '_experimental_nc',
-		panel = 'surface',
-		panel_nc = 'base',
-		border = 'highlight_med',
-		comment = 'muted',
-		link = 'iris',
-		punctuation = 'subtle',
+    --- @usage string hex value or named color from rosepinetheme.com/palette
+    groups = {
+        background = 'base',
+        background_nc = '_experimental_nc',
+        panel = 'surface',
+        panel_nc = 'base',
+        border = 'highlight_med',
+        comment = 'muted',
+        link = 'iris',
+        punctuation = 'subtle',
 
-		error = 'love',
-		hint = 'iris',
-		info = 'foam',
-		warn = 'gold',
+        error = 'love',
+        hint = 'iris',
+        info = 'foam',
+        warn = 'gold',
 
-		headings = {
-			h1 = 'iris',
-			h2 = 'foam',
-			h3 = 'rose',
-			h4 = 'gold',
-			h5 = 'pine',
-			h6 = 'foam',
-		}
-		-- or set all headings at once
-		-- headings = 'subtle'
-	},
+        headings = {
+            h1 = 'iris',
+            h2 = 'foam',
+            h3 = 'rose',
+            h4 = 'gold',
+            h5 = 'pine',
+            h6 = 'foam',
+        }
+        -- or set all headings at once
+        -- headings = 'subtle'
+    },
 
-	-- Change specific vim highlight groups
-	-- https://github.com/rose-pine/neovim/wiki/Recipes
-	highlight_groups = {
-		ColorColumn = { bg = 'rose' },
+    -- Change specific vim highlight groups
+    -- https://github.com/rose-pine/neovim/wiki/Recipes
+    highlight_groups = {
+        ColorColumn = { bg = 'rose' },
 
-		-- Blend colours against the "base" background
-		CursorLine = { bg = 'foam', blend = 10 },
-		StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+        -- Blend colours against the "base" background
+        CursorLine = { bg = 'foam', blend = 10 },
+        StatusLine = { fg = 'love', bg = 'love', blend = 10 },
 
-		-- By default each group adds to the existing config.
-		-- If you only want to set what is written in this config exactly,
-		-- you can set the inherit option:
-		Search = { bg = 'gold', inherit = false },
-	}
+        -- By default each group adds to the existing config.
+        -- If you only want to set what is written in this config exactly,
+        -- you can set the inherit option:
+        Search = { bg = 'gold', inherit = false },
+    }
 })
 
 -- Set colorscheme after options
