d2a54b9dedb5c5fa97fb4763283a2fc0b4b50e87
connormullin <connorjohnmullin@gmail.com>
2023-09-05 23:45:08 -0600

    setting up plugins
    set up tsserver properly tailwindcss changed color scheme

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,52 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = { "jose-elias-alvarez/typescript.nvim" },
+  opts = {
+    -- make sure mason installs the server
+    servers = {
+      tsserver = {
+        keys = {
+          { "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", desc = "Organize Imports" },
+          { "<leader>cR", "<cmd>TypescriptRenameFile<CR>", desc = "Rename File" },
+        },
+        settings = {
+          typescript = {
+            format = {
+              indentSize = vim.o.shiftwidth,
+              convertTabsToSpaces = vim.o.expandtab,
+              tabSize = vim.o.tabstop,
+            },
+          },
+          javascript = {
+            format = {
+              indentSize = vim.o.shiftwidth,
+              convertTabsToSpaces = vim.o.expandtab,
+              tabSize = vim.o.tabstop,
+            },
+          },
+          completions = {
+            completeFunctionCalls = true,
+          },
+        },
+      },
+
+      tailwindcss = {
+        filetypes_exclude = { "markdown" },
+      },
+    },
+    setup = {
+      tsserver = function(_, opts)
+        require("typescript").setup({ server = opts })
+        return true
+      end,
+
+      tailwindcss = function(_, opts)
+        local tw = require("lspconfig.server_configurations.tailwindcss")
+        --- @param ft string
+        opts.filetypes = vim.tbl_filter(function(ft)
+          return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+        end, tw.default_config.filetypes)
+      end,
+    },
+  },
+}
