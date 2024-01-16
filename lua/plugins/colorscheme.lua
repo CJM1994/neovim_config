8365ca8febcfde328fd74398b55fff09340b1e80
connormullin <connorjohnmullin@gmail.com>
2024-01-16 01:32:34 -0700

    install markdown-preview & change color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,66 +1,16 @@
 return {
   {
+    { "ellisonleao/gruvbox.nvim" },
+    { "shaunsingh/nord.nvim" },
+    { "kvrohit/rasmus.nvim" },
+    { "Tsuzat/NeoSolarized.nvim" },
+    { "rose-pine/neovim" },
+    { "savq/melange-nvim" },
+    { "rebelot/kanagawa.nvim" },
     {
-      { "ellisonleao/gruvbox.nvim" },
-      {
-        "LazyVim/LazyVim",
-        opts = {
-          colorscheme = "gruvbox",
-        },
-      },
-    },
-    {
-      { "shaunsingh/nord.nvim" },
-      {
-        "LazyVim/LazyVim",
-        opts = {
-          colorscheme = "nord",
-        },
-      },
-    },
-    {
-      { "rebelot/kanagawa.nvim" },
-      {
-        "LazyVim/LazyVim",
-        opts = {
-          colorscheme = "kanagawa",
-        },
-      },
-    },
-    {
-      { "kvrohit/rasmus.nvim" },
-      {
-        "LazyVim/LazyVim",
-        opts = {
-          colorscheme = "rasmus",
-        },
-      },
-    },
-    {
-      { "Tsuzat/NeoSolarized.nvim" },
-      {
-        "LazyVim/LazyVim",
-        opts = {
-          colorscheme = "NeoSolarized",
-        },
-      },
-    },
-    {
-      { "rose-pine/neovim" },
-      {
-        "LazyVim/LazyVim",
-        opts = {
-          colorscheme = "rose-pine-moon",
-        },
-      },
-    },
-    {
-      { "savq/melange-nvim" },
-      {
-        "LazyVim/LazyVim",
-        opts = {
-          colorscheme = "melange",
-        },
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "rose-pine-moon",
       },
     },
   },
